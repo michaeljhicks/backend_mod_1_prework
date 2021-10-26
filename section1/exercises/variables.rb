1a57afd2e8e5252cde43ad2279706d535d5df92a
Michael Hicks <hicks.michael04@gmail.com>
2021-10-26 16:05:38 -0600

    Add Section 1

--- section1/exercises/variables.rb
+++ section1/exercises/variables.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section1/exercises/variables.rb`
 
 # Example: Write code that saves your name to a variable and
@@ -11,49 +11,57 @@ p name
 # Write code that saves the string 'Dobby' to a variable and
 # prints what that variable holds to the terminal:
 house_elf = "Dobby"
-# YOUR CODE HERE
+p house_elf
 
 # Write code that saves the string 'Harry Potter must not return to Hogwarts!'
 # and prints what that variable holds to the terminal:
-# YOUR CODE HERE
+harry_potter_return = "Harry Potter must not return to Hogwarts!"
+p harry_potter_return
 
 # Write code that adds 2 to the `students` variable and
 # prints the result:
 students = 22
-# YOUR CODE HERE
+students += 2
 p students
 
 # Write code that subracts 2 from the `students` variable and
 # prints the result:
-# YOUR CODE HERE
+students -= 2
 p students
 
 
 # YOU DO:
-# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`. 
+# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`.
 # Store the appropriate data types in each.
 # print all three variables to the terminal.
-
+first_name = "Rosie"
+is_hungry = true
+number_of_pets = 2
+p "Your pet #{first_name} is #{is_hungry} and you have a total of #{number_of_pets} pets."
 # IN WORDS:
-# How did you decide to use the data type you did for each of the three variables above? 
-
+# How did you decide to use the data type you did for each of the three variables above?
 # Explain.
+# Answer: Simply put; the exercise was requiring a name (string), a boolean, and an integer.
 
 
 # YOU DO:
 # Re-assign the values to the three variables from the previous challenge to different values (but same data type).
 # print all three variables to the terminal.
-
-
+first_name = "Ada"
+is_hungry = false
+number_of_pets = 1
+p "Your pet #{first_name} is #{is_hungry} and you have a total of #{number_of_pets} pets."
 # YOU DO:
 # Using the variables below, print the total number of snacks to the terminal:
 healthy_snacks = 6;
 junk_food_snacks = 8;
-
+total_snacks = healthy_snacks + junk_food_snacks
+p total_snacks
 
 #-------------------
 # FINAL CHECK
 #-------------------
 
-# Did you run this file in your terminal to make sure everything printed out to the terminal 
-  # as you would expect? 
+# Did you run this file in your terminal to make sure everything printed out to the terminal
+  # as you would expect?
+  # Answer: Yes, but I'm not certain that I'm using the boolean values correctly when I print all three variables above.
